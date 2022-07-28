b127001fc655a0f3939354fac3a5b2ed61f3b224
inotomo06 <86100981+inotomo06@users.noreply.github.com>
2022-07-28 15:02:50 +0900

    Initial Commit

---
+++ AngleSensor/AngleSensorApp.swift
@@ -0,0 +1,17 @@
+//
+//  AngleSensorApp.swift
+//  AngleSensor
+//
+//  Created by cmStudent on 2022/07/28.
+//
+
+import SwiftUI
+
+@main
+struct AngleSensorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
